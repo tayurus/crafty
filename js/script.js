e4e6354258ec667bb25b3b6b1ff1fa77993b9949
tayurus <tayurus@gmail.com>
2018-01-06 00:39:44 +0300

    about block was added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  $(".owl-carousel").owlCarousel({
-      items:1,
-      dots:true
-  })
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        dots: true
+    })
+
+    setInterval(() => {
+        $(".about-item img:not(:first)").each((index, item) => {
+            setTimeout(() => {
+                $(item).css({
+                    "background-color": "#ffde00"
+                })
+            }, (index + 1) * 1000);
+        });
+        setTimeout(() => {
+            $(".about-item img").css({
+                    "background-color": "transparent"
+                })
+            }, 4000);
+    }, 4000);
+
+
+
+
 });
